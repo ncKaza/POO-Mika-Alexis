31ff6d4f2205916a3472b5c861b777e44c484f7f
ncKaza <alexis.kazamiz@gmail.com>
2026-09-21 16:14:06 +1100

    modif de Lundi 21/09

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,36 +11,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("NC EXPLORER 2026"),
-        ),
-        body:  Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:[
-              SvgPicture.asset("assets/images/NC_EXPLORER_logo.svg"),
-              Image.asset("assets/images/LOGO.png"),
-              const Text(
-                  "NC EXPLORER l'application numéro 1 du Caillou",
-                style: TextStyle(
-                  fontSize: 36,
-                  fontFamily: 'Poppins'
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const Text("Découvrez le Caillou sous tout ses cotés ",
-              style: TextStyle(
-                fontSize: 24
-              ),
-                textAlign: TextAlign.center,
-              )
-            ],
-          ),
-        )
-      ),
+      home: HomePage(),
     );
   }
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("NC EXPLORER 2026"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/LOGO.png"),
+            const Text(
+              "NC EXPLORER l'application numéro 1 du Caillou",
+              style: TextStyle(
+                fontSize: 36,
+                fontFamily: 'Poppins',
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Text(
+              "Découvrez le Caillou sous tout ses cotés",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+            ElevatedButton(
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.green),
+              ),
+              onPressed: () => print("clickez ici"),
+              child: const Text(
+                "Affichez la carte",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
